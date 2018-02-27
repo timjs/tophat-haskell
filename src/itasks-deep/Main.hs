a16bef27bd1e15b4612d28f345e2c7f7752dac07
Markus Klinik <mkl@lambdanaut.net>
2018-02-27 17:02:52 +0100

    separate stepping and evaluation

--- src/itasks-deep/Main.hs
+++ src/itasks-deep/Main.hs
@@ -4,7 +4,35 @@ module Main where
 -- are two semantics, the evaluation semantics and the user interface
 -- semantics. No stable/unstable values yet. Therefore ParOr is not possible.
 
+-- If we follow the principle of deep embedding strictly, then all semantics
+-- should be separated. There are three different semantics: The step
+-- semantics, the value semantics and the ui semantics. They come as separate
+-- functions which _interpret_ tasks.
+--
+--   TaskTree -> Value
+--   TaskTree -> UI
+--   TaskTree -> Event -> TaskTree
+--
+-- In a shallow embedding, all semantics are combined into one data type, which
+-- _is_ the task. The type must be recursive.
+--
+--   data Task = Task (Value, UI, Event -> Task)
+--
+-- Note how the arrow of the step semantics is inside the tuple.
+--
+-- Bas says: the real iTasks implementation is neither deep nor shallow, it
+-- uses a different technique. Tasks never change, they never rewrite. TaskTree
+-- is not the same as the AST of a deep embedding.
+-- iTasks is shallow-ish in the sense that the arrow _is_ the task.
+--
+--   :: Task = Task (Event TaskTree -> (Value, TaskTree))
+--
+-- All tasks are interpreters of their TaskTree. The structure of a whole
+-- workflow is fixed. It re-routes events as steps are being taken.
+
 type ID = String
+
+type UI = String
 
 data Value
   = NoValue -- required for step combinator
@@ -24,37 +52,37 @@ data Event
 
 
 -- Evaluation semantics
-sem :: Event -> TaskTree -> (Value, TaskTree) -- This arrow is completely defined by the semantics
+value :: TaskTree -> Value
+value (Editor _ v) = v
+value (Bind _ _ _) = NoValue
+value (ParAnd lhs rhs) = JustValue $ "(" ++ show (value lhs) ++ "," ++ show (value rhs) ++ ")"
+
+
+-- Step semantics
+step :: Event -> TaskTree -> TaskTree -- This arrow is completely defined by the semantics
 
 -- Editors change their value with an appropriate event
-sem (EditorEvent evId newVal) (Editor taskId oldVal)
-  | evId == taskId = (newVal, Editor taskId newVal)
-sem _ t@(Editor _ oldVal) = (oldVal, t)
+step (EditorEvent evId newVal) (Editor taskId _)
+  | evId == taskId = Editor taskId newVal
+step _ t@(Editor _ _) = t
 
 -- Bind takes a step when its button is pressed
-sem e@(StepEvent evId) (Bind taskId lhs rhs)
-  | evId == taskId =
-    -- This is a bit ugly, because to get the value of the left hand side, we
-    -- must send it some event. By assumption all IDs are unique, so lhs should
-    -- not react to the event. In iTasks they use refresh events if I remember
-    -- correctly.
-    let (leftVal, _) = sem e lhs in
-    (NoValue, rhs leftVal)
-sem e (Bind taskId lhs rhs) =
-    let (_, newLhs) = sem e lhs in
-    (NoValue, Bind taskId newLhs rhs)
+step e@(StepEvent evId) (Bind taskId lhs rhs)
+  | evId == taskId = rhs (value lhs) -- The step semantics uses the value semantics. Weird.
+step e (Bind taskId lhs rhs) =
+    Bind taskId (step e lhs) rhs
 
 -- Parallel distributes the event to the subtasks
-sem e (ParAnd lhs rhs) =
+step e (ParAnd lhs rhs) =
   let
-    (lVal, newLhs) = sem e lhs
-    (rVal, newRhs) = sem e rhs
+    newLhs = step e lhs
+    newRhs = step e rhs
   in
-    (JustValue $ "(" ++ show lVal ++ "," ++ show rVal ++ ")", ParAnd newLhs newRhs)
+    ParAnd newLhs newRhs
 
 
 -- User interface semantics
-ui :: TaskTree -> String
+ui :: TaskTree -> UI
 ui (Editor id val) = "editor " ++ id ++ " " ++ show val ++ "\n"
 ui (Bind id lhs _) = ui lhs ++ "step " ++ id ++ "\n"
 ui (ParAnd lhs rhs) = ui lhs ++ ui rhs
@@ -63,7 +91,7 @@ ui (ParAnd lhs rhs) = ui lhs ++ ui rhs
 runTask task = do
   putStrLn $ ui task
   ev <- getEvent
-  runTask $ snd $ sem ev task
+  runTask $ step ev task
 
 
 -- "ed 0 fooo" for an editor event with id 0 and value foo
