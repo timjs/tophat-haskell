e38f9c29517d5fd9f142749776f8d9ac245a9150
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2018-10-04 19:50:20 +0200

    add Surely

---
+++ src/Data/Surely.hs
@@ -0,0 +1,28 @@
+module Data.Surely where
+
+
+import Base
+
+
+-- | A `Surely a` represents an input value.
+-- | It can be exactly the value `a`,
+-- | or anything which matches the type of `a`.
+-- |
+-- | It is similar to `Maybe a`,
+-- | except that `Anything` is equal to every exact value.
+-- | That means that `Semantics.inputs` can produce `Change` inputs
+-- | without specifying a specific input value.
+data Surely a
+  = Exactly a
+  | Anything
+  deriving (Show, Eq, Ord, Functor)
+
+
+toMaybe :: Surely a -> Maybe a
+toMaybe (Exactly x) = Just x
+toMaybe (Anything)  = Nothing
+
+
+toSurely :: Maybe a -> Surely a
+toSurely (Just x)  = Exactly x
+toSurely (Nothing) = Anything
