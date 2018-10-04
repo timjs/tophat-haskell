b5293bcea512f1cf8061a0324590aead51acab05
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2018-10-04 20:36:30 +0200

    add properties on pairing

---
+++ src/Data/Task/Equality.hs
@@ -0,0 +1,51 @@
+module Data.Task.Equality where
+
+
+import Base
+
+import Data.Task
+
+import System.IO.Unsafe
+
+
+infix 4 ===
+
+
+
+-- Equivallence ----------------------------------------------------------------
+
+
+(===) :: Basic a => MonadRef l m => TaskT l m a -> TaskT l m a -> m Bool
+t1 === t2 = do
+  t1' <- normalise t1
+  t2' <- normalise t2
+  v1 <- value t1'
+  v2 <- value t2'
+  pure $ v1 == v2
+
+
+
+-- Properties ------------------------------------------------------------------
+
+
+prop_pair_left_identity :: Task Int -> Bool
+prop_pair_left_identity t = unsafePerformIO $
+  tmap snd (edit () |&| t) === t
+
+
+prop_pair_right_identity :: Task Int -> Bool
+prop_pair_right_identity t = unsafePerformIO $
+  tmap fst (t |&| edit ()) === t
+
+
+prop_pair_associativity :: Task Int -> Task Int -> Task Int -> Bool
+prop_pair_associativity r s t = unsafePerformIO $
+  tmap assoc (r |&| (s |&| t)) === (r |&| s) |&| t
+
+
+
+-- Helpers --
+
+
+assoc :: ( a, ( b, c ) ) -> ( ( a, b ), c )
+assoc ( a, ( b, c ) ) = ( ( a, b ), c )
--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,7 @@ module Main where
 import Protolude
 
 import Data.Task
-import Data.Task.Internal ()
+import Data.Task.Equality
 
 import System.IO.Unsafe
 
@@ -13,8 +13,11 @@ import Test.QuickCheck
 
 main :: IO ()
 main = do
-  verboseCheck prop_equal_val
-  verboseCheck prop_normalising_preserves_failing
+  quickCheck prop_equal_val
+  quickCheck prop_normalising_preserves_failing
+  quickCheck prop_pair_left_identity
+  quickCheck prop_pair_right_identity
+  quickCheck prop_pair_associativity
   where
 
     prop_equal_val :: Task Int -> Bool
@@ -27,10 +30,10 @@ main = do
     prop_normalising_preserves_failing t = unsafePerformIO $ do
       t' <- normalise t
       pure $ failing t == failing t'
-      
-    prop_norm_val :: Task Int -> Bool
-    prop_norm_val t = unsafePerformIO $ do
-      v <- value t
-      t' <- normalise t
-      v' <- value t'
-      pure $ v == v'
+
+    -- prop_norm_val :: Task Int -> Bool
+    -- prop_norm_val t = unsafePerformIO $ do
+    --   v <- value t
+    --   t' <- normalise t
+    --   v' <- value t'
+    --   pure $ v == v'
