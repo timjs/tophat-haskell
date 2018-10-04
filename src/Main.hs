2a373bf930de62855a4fab8d6894d8d2be8269c1
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2018-10-04 14:43:07 +0200

    add first test!

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,22 @@
 module Main where
 
+
 import Protolude
 
+import Data.Task
+import Data.Task.Internal ()
+
+import System.IO.Unsafe
+
+import Test.QuickCheck
+
+
 main :: IO ()
-main = do
-  putText "hello world"
+main =
+  quickCheck prop_equal_values
+  where
+    prop_equal_values :: Task Int -> Bool
+    prop_equal_values t = unsafePerformIO $ do
+      x <- value t
+      y <- value t
+      pure $ x == y
