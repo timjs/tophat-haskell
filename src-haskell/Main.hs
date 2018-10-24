00ca72a4b1a267c4d796046080a2f0845eca42c7
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2018-10-24 14:39:15 +0200

    more quickchecks

--- src-haskell/Main.hs
+++ src-haskell/Main.hs
@@ -3,10 +3,7 @@ module Main where
 
 import Protolude
 
-import Data.Task
 import Data.Task.Equality
-
-import System.IO.Unsafe
 
 import Test.QuickCheck
 
@@ -16,43 +13,26 @@ main = do
   putText ">>> Valid properties:"
 
   quickCheck prop_equal_val
-  quickCheck prop_normalising_preserves_failing
+  quickCheck prop_failing_preserved
 
   quickCheck prop_pair_left_identity
   quickCheck prop_pair_right_identity
   quickCheck prop_pair_associativity
+  quickCheck prop_pair_swap
 
   quickCheck prop_choose_left_identity
   quickCheck prop_choose_right_identity
   quickCheck prop_choose_associativity
   quickCheck prop_choose_left_absorbtion
   quickCheck prop_choose_left_catch
+  quickCheck prop_choose_idempotent
 
   quickCheck prop_step_left_identity
   quickCheck prop_step_right_identity
   quickCheck prop_step_assocaitivity
+  quickCheck prop_step_left_anihilation
 
   putText ""
   putText ">>> Invalid properties:"
 
   quickCheck prop_choose_not_commutative
-
-  where
-
-    prop_equal_val :: TaskIO Int -> Bool
-    prop_equal_val t = unsafePerformIO $ do
-      x <- value t
-      y <- value t
-      pure $ x == y
-
-    prop_normalising_preserves_failing :: TaskIO Int -> Bool
-    prop_normalising_preserves_failing t = unsafePerformIO $ do
-      t' <- normalise t
-      pure $ failing t == failing t'
-
-    -- prop_norm_val :: Task Int -> Bool
-    -- prop_norm_val t = unsafePerformIO $ do
-    --   v <- value t
-    --   t' <- normalise t
-    --   v' <- value t'
-    --   pure $ v == v'
