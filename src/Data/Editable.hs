30d1a23e85d01d02d71f9c1af532926021e475e7
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2019-08-19 10:08:20 +0200

    make Editable a class synonym for better error messages

--- src/Data/Editable.hs
+++ src/Data/Editable.hs
@@ -3,4 +3,13 @@ module Data.Editable
   ) where
 
 
-type Editable a = ( Pretty a, Read a, Eq a, Typeable a ) --, Arbitrary a ) --, Coarbitrary a )
+-- | Types `a` which can be edited by an end user.
+-- |
+-- | They should be:
+-- | * `Pretty` to show them in a user interface.
+-- | * `Read` to parse them from input.
+-- | * `Typeable` to match input type and editor type witch each other.
+-- |
+-- | FIXME: Maybe also 'Arbitrary' and `Coarbitrary` ?
+class ( Pretty a, Read a, Eq a, Typeable a ) => Editable a
+instance ( Pretty a, Read a, Eq a, Typeable a ) => Editable a
