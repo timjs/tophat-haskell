d0dcacbd1fa162cb1581038f59987f0cc8320506
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2019-09-23 13:47:52 +0200

    simplify status definition

--- src/Data/Task/Status.hs
+++ src/Data/Task/Status.hs
@@ -3,113 +3,94 @@ module Data.Task.Status where
 import Data.Task (Task(..), Collaborative(..))
 
 
--- Status ----------------------------------------------------------------------
+-- Definition ------------------------------------------------------------------
 
+-- | The status of a `Task a` is defined to be one of four options:
 data Status a
-  = Failing
-  | Looping
-  | Stepping
-  | Asking
-  | Terminating a
-  deriving ( Show, Eq, Ord, Functor )
+  = Producing a  -- ^ it is producing a value of type `a` (i.e. valued and shared editors)
+  | Asking       -- ^ it is asking for a value of type `a` (i.e. empty editors)
+  | Failing      -- ^ it is failing, and always will be failing (i.e. the failing task)
+  | Looping      -- ^ it is looping due to a forever running task or a subtask which is looping or failing
+  | Stepping     -- ^ it is still stepping, and we do not know how it will end
+  deriving ( Show, Functor )
+
+
+-- Equality --------------------------------------------------------------------
+
+-- | Status observations _of the same type_ are equal in only three cases:
+instance Eq a => Eq (Status a) where
+  Producing x == Producing y = x == y  -- ^ when the values produced are equal
+  Asking      == Asking      = True    -- ^ when both ask for a value
+  Failing     == Failing     = True    -- ^ when both are `Failing`
+  _           == _           = False
+  -- ^ in any other case, tasks are looping or stepping and we do not know how things will end.
+
+
+-- Combinations ----------------------------------------------------------------
 
 instance Monoidal Status where
-  skip = Terminating ()
+  skip = Producing ()
 
-  Terminating x <&> Terminating y = Terminating ( x, y )
-  Failing       <&> _             = Failing
-  _             <&> Failing       = Failing
-  Looping       <&> _             = Looping
-  _             <&> Looping       = Looping
-  Stepping      <&> _             = Stepping
-  _             <&> Stepping      = Stepping
-  Asking        <&> _             = Asking
-  _             <&> Asking        = Asking
+  -- | The parallel combination of two `Status`es is simple when both tasks are `Producing` a value, then we create a pair.
+  Producing x <&> Producing y = Producing ( x, y )
+  -- | When both tasks are `Asking` for a value, we could as well ask for a pair immediately.
+  Asking      <&> Asking      = Asking
+  -- | When both tasks are failing, we could as well fail immediately.
+  Failing     <&> Failing     = Failing
+  -- | When one of the two tasks is looping, the combination is looping.
+  Looping     <&> _           = Looping
+  _           <&> Looping     = Looping
+  -- | Otherwise we are still stepping, and do not know how things will end.
+  _           <&> _           = Stepping
 
 instance Applicative Status where
   pure  = pureDefault
   (<*>) = applyDefault
 
-{-
-instance Applicative Status where
-  Terminating f <*> Terminating x = Terminating (f x)
-  Terminating _ <*> Failing     = Failing
-  Terminating _ <*> Looping     = Looping
-  Terminating _ <*> Stepping    = Stepping
-  Terminating _ <*> Asking      = Asking
-  Failing     <*> _           = Failing
-  Looping     <*> _           = Looping
-  Stepping    <*> _           = Stepping
-  Asking      <*> _           = Asking
-
-  pure = Terminating
-
-instance Monoidal Status
--}
-
 instance Alternative Status where
-  empty = Failing
+  -- | `Stepping` is the status we know the least about.
+  empty = Stepping
 
-  Terminating x <|> _             = Terminating x
-  _             <|> Terminating y = Terminating y
-  _             <|> _             = Failing  --XXX
+  -- | The alternative combination of two `Status`es picks the first to have a value.
+  -- | It doesn't matter if one is looping or not.
+  Producing x <|> _           = Producing x
+  _           <|> Producing y = Producing y
+  -- | Otherwise, when one of the two tasks is looping, we know we will loop while making a choice.
+  Looping     <|> _           = Looping
+  _           <|> Looping     = Looping
+  -- | Otherwise we are still stepping, and do not know which choice to make.
+  _           <|> _           = Stepping
 
 
 -- Observation -----------------------------------------------------------------
 
 status :: Collaborative r m => Task m a -> m (Status a)
 status = \case
-  Done v       -> pure (Terminating v)
-  Update v     -> pure (Terminating v)
-  View v       -> pure (Terminating v)
+  -- | Valued editors are `Producing` a value...
+  Done v       -> pure (Producing v)
+  Update v     -> pure (Producing v)
+  View v       -> pure (Producing v)
 
-  Share v      -> pure Terminating -< share v
-  Assign _ _   -> pure (Terminating ())
-  Change l     -> pure Terminating -< watch l
-  Watch l      -> pure Terminating -< watch l
+  -- | ...as do shared editors.
+  Share v      -> pure Producing -< share v
+  Assign _ _   -> pure (Producing ())
+  Change l     -> pure Producing -< watch l
+  Watch l      -> pure Producing -< watch l
 
+  -- | Unvalued editors are `Asking` for a value.
+  Enter        -> pure Asking
+
+  -- | Fail is obviously `Failing`...
+  Fail         -> pure Failing
+
+  -- | ...and loops are `Looping`.
+  Forever _    -> pure Looping
+
+  -- | Combinations pair or choose one of the possibilities.
   Pair t1 t2   -> pure (<&>) -< status t1 -< status t2
   Choose t1 t2 -> pure (<|>) -< status t1 -< status t2
   Trans f t    -> pure (map f) -< status t
 
-  Fail         -> pure Failing
-
-  Forever _    -> pure Looping
-
-  Step _ _     -> pure Stepping
+  -- | From the rest we do not know anything.
   Pick _       -> pure Stepping
-
-  Enter        -> pure Asking
-
-{-
-instance Eq a => Eq (Task m a) where
-  -- We can only equate values in a `Done` iff their contents are `Eq`.
-  Done v1        == Done v2        = v1 == v2
-  -- Empty editors are equal when their types match.
-  Enter          == Enter          = True
-  -- Editors are equal when their contents are equal...
-  Update v1      == Update v2      = v1 == v2
-  -- ...same holds for view only editors.
-  View v1        == View v2        = v1 == v2
-  -- FIXME: What to do with picks?
-  Pick ts1       == Pick ts2       = undefined
-  -- Pairs are equal when their parts are equal...
-  Pair t11 t12   == Pair t21 t22   = t11 == t21 && t12 == t22
-  -- ...same holds for choices.
-  Choose t11 t12 == Choose t21 t22 = t11 == t21 && t12 == t22
-  -- Two `Fail`s are always equal when the types match.
-  Fail           == Fail           = True
-  -- FIXME: What to do with transformations?
-  Trans f1 t1    == Trans f2 t2    = undefined
-  -- Steps can never be equal, comparing continuations is not possible.
-  Step _ _       == Step _ _       = False
-  -- Loops are equal when their parts are equal.
-  Forever t1     == Forever t2     = t1 == t2
-  -- Shares of equal values are equal.
-  Share v1       == Share v2       = v1 == v2
-  Assign v1 l1   == Assign v2 l2   = v1 == v2 && l1 == l2
-  -- Shared editors are equal when they watch the same location...
-  Watch l1       == Watch l2       = l1 == l2
-  -- ...same holds for view only shared editors.
-  Change l1      == Change l2      = l1 == l2
--}
+  Step _ _     -> pure Stepping
