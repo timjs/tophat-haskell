328fd3b6dfd2b523511d30cb19d14010f723f034
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2020-04-16 16:52:02 +0200

    refactor labels out of actions

--- src/Data/Task/Input.hs
+++ src/Data/Task/Input.hs
@@ -1,15 +1,13 @@
 {-# LANGUAGE ViewPatterns #-}
+
 module Data.Task.Input
-  ( Action (..),
-    Dummy (..),
+  ( Concrete (..),
     Symbolic (..),
+    Dummy,
+    dummy,
     Input (..),
-    pattern Named,
-    pattern Unnamed,
-    pattern Labeled,
-    pattern Unlabeled,
-    dummyfy,
-    strip,
+    pattern ISelect,
+    pattern IPreselect,
     usage,
     parse,
   )
@@ -22,115 +20,111 @@ import qualified Data.Text.Prettyprint.Doc as Pretty
 
 -- Actions ---------------------------------------------------------------------
 
--- Real actions --
+-- Concrete actions --
 
-data Action :: Type where
-  IEnter :: Editable b => b -> Action
-  ISelect :: Label -> Action
+data Concrete :: Type where
+  Concrete :: Editable b => b -> Concrete
 
-instance Eq Action where
-  IEnter x == IEnter y
+instance Eq Concrete where
+  Concrete x == Concrete y
     | Just Refl <- x ~= y = x == y
     | otherwise = False
-  ISelect x == ISelect y = x == y
   _ == _ = False
 
-instance Pretty Action where
+instance Pretty Concrete where
   pretty = \case
-    IEnter x -> pretty x
-    ISelect l -> pretty l
-
--- Dummy actions --
-
-data Dummy :: Type where
-  AEnter :: Editable b => Proxy b -> Dummy
-  ASelect :: Label -> Dummy
-
-instance Eq Dummy where
-  AEnter x == AEnter y
-    -- NOTE: We're comparing proxies, they are always equal when the types are equal.
-    | Just Refl <- x ~= y = True
-    | otherwise = False
-  ASelect x == ASelect y = x == y
-  _ == _ = False
-
-instance Pretty Dummy where
-  pretty = \case
-    AEnter p -> cat ["<", pretty beta, ">"]
-      where
-        beta = typeOfProxy p
-    ASelect l -> pretty l
+    Concrete x -> pretty x
 
 -- Symbolic actions --
 
 data Symbolic :: Type where
-  SEnter :: Editable b => Proxy b -> Symbolic
-  SSelect :: Label -> Symbolic
+  Symbolic :: Editable b => Proxy b -> Symbolic
+
+instance Eq Symbolic where
+  Symbolic x == Symbolic y
+    -- NOTE: We're comparing proxies, they are always equal when the types are equal.
+    | Just Refl <- x ~= y = True
+    | otherwise = False
+  _ == _ = False
+
+instance Pretty Symbolic where
+  pretty = \case
+    Symbolic p -> cat ["<", pretty beta, ">"]
+      where
+        beta = typeOfProxy p
+
+-- Dummy actions --
+
+type Dummy = Symbolic
+
+dummy :: Editable b => Proxy b -> Dummy
+dummy p = Symbolic p
 
 -- Inputs ----------------------------------------------------------------------
 
-data Input a
-  = Send Nat a
-  | Prepare Label
+data Input b
+  = IEnter Nat b
+  | IOption Name Label
   deriving (Eq, Show, Functor, Foldable, Traversable)
 
-instance Pretty a => Pretty (Input a) where
+{-# COMPLETE IEnter, ISelect, IPreselect #-}
+pattern ISelect :: Nat -> Label -> Input b
+pattern ISelect n l = IOption (Named n) l
+
+pattern IPreselect :: Label -> Input b
+pattern IPreselect l = IOption Unnamed l
+
+instance Pretty b => Pretty (Input b) where
   pretty = \case
-    Send n a -> sep [pretty n, pretty a]
-    Prepare l -> pretty l
+    IEnter n b -> cat [pretty b, "^", pretty n]
+    ISelect n l -> cat [pretty l, "^", pretty n]
+    IPreselect l -> pretty l
 
-{-# COMPLETE Named, Unnamed #-}
-pattern Named :: Nat -> Input Action
-pattern Named n <- (name -> Just n)
+-- -- Action view --
 
-pattern Unnamed :: Input Action
-pattern Unnamed <- (name -> Nothing)
+-- data Action b
+--   = AValue b
+--   | ALabel Label
 
-name :: Input Action -> Maybe Nat
-name = \case
-  Send n _ -> Just n
-  Prepare _ -> Nothing
+-- {-# COMPLETE ISend, IPreselect #-}
+-- pattern ISend :: Nat -> Action b -> Input b
+-- pattern ISend n a <- (action -> Just (n, a))
 
-{-# COMPLETE Labeled, Unlabeled #-}
-pattern Labeled :: Label -> Input Action
-pattern Labeled l <- (label -> Just l)
-
-pattern Unlabeled :: Input Action
-pattern Unlabeled <- (label -> Nothing)
-
-label :: Input Action -> Maybe Label
-label = \case
-  Send _ (IEnter _) -> Nothing
-  Send _ (ISelect l) -> Just l
-  Prepare l -> Just l
+-- action:: Input b -> Maybe (Nat, Action b)
+-- action= \case
+--   IEnter n b -> Just (n, AValue b)
+--   ISelect n l -> Just (n, ALabel l)
+--   IPreselect _ -> Nothing
 
 -- Conformance -----------------------------------------------------------------
 
-dummyfy :: Action -> Dummy
-dummyfy = \case
-  IEnter x -> AEnter (proxyOf x)
-  ISelect l -> ASelect l
+-- dummyfy :: Action -> Dummy
+-- dummyfy = \case
+--   IEnter x -> AEnter (proxyOf x)
+--   ISelect l -> ASelect l
 
 -- reify :: Dummy -> Gen (List Action)
 -- reify (AEnter l)  = map IEnter <$> vectorOf 5 (arbitraryOf l)
 -- reify (ASelect l)   = pure [ ISelect l ]
 
-strip :: Input Action -> Input Dummy
-strip = map dummyfy
+-- strip :: Input Action -> Input Dummy
+-- strip = map dummyfy
 
 -- fill :: Input Dummy -> Gen (List (Input Action))
 -- fill = map sequence << sequence << map reify
 
 -- Parsing ---------------------------------------------------------------------
 
+--TODO fix parser
 usage :: Doc n
 usage =
   split
     [ ":: Possible inputs are:",
-      "    <value>@<id> : enter <value> into the current editor",
-      "    <label>@<id> : select one of the possible options",
-      "    h(elp)       : show this message",
-      "    q(uit)       : quit",
+      "    <id> <value> : enter <value> into editor <id>",
+      "    <id> <label> : select one of the possible options from editor <id>",
+      "    <label>      : continue with one of the possible options",
+      "    help, h      : show this message",
+      "    quit, q      : quit",
       "",
       "where ids have the form:",
       "    2, 37, …",
@@ -152,30 +146,32 @@ parseId t
   | Just v <- scan t :: Maybe Nat = ok v
   | otherwise = throw <| sep ["!!", Pretty.dquotes <| pretty t, "is not a proper id"]
 
-parseLabel :: Text -> Either (Doc n) Action
+parseLabel :: Text -> Either (Doc n) Label
 parseLabel t
-  | Just (c, _) <- Text.uncons t, Char.isUpper c = ok <| ISelect t
+  | Just (c, _) <- Text.uncons t, Char.isUpper c = ok <| t
   | otherwise = throw <| sep ["!!", Pretty.dquotes <| pretty t, "is not a proper label"]
 
-parseVal :: Text -> Either (Doc n) Action
-parseVal val
-  | Just v <- scan val :: Maybe Unit = ok <| IEnter v
-  | Just v <- scan val :: Maybe Bool = ok <| IEnter v
-  | Just v <- scan val :: Maybe Int = ok <| IEnter v
-  | Just v <- scan val :: Maybe Double = ok <| IEnter v
-  | Just v <- scan val :: Maybe Text = ok <| IEnter v
-  | Just v <- scan val :: Maybe [Bool] = ok <| IEnter v
-  | Just v <- scan val :: Maybe [Int] = ok <| IEnter v
-  | Just v <- scan val :: Maybe [Double] = ok <| IEnter v
-  | Just v <- scan val :: Maybe [Text] = ok <| IEnter v
+parseConcrete :: Text -> Either (Doc n) Concrete
+parseConcrete val
+  | Just v <- scan val :: Maybe Unit = ok <| Concrete v
+  | Just v <- scan val :: Maybe Bool = ok <| Concrete v
+  | Just v <- scan val :: Maybe Int = ok <| Concrete v
+  | Just v <- scan val :: Maybe Double = ok <| Concrete v
+  | Just v <- scan val :: Maybe Text = ok <| Concrete v
+  | Just v <- scan val :: Maybe [Bool] = ok <| Concrete v
+  | Just v <- scan val :: Maybe [Int] = ok <| Concrete v
+  | Just v <- scan val :: Maybe [Double] = ok <| Concrete v
+  | Just v <- scan val :: Maybe [Text] = ok <| Concrete v
   | otherwise = throw <| sep ["!! Error parsing value", Pretty.dquotes (pretty val)]
 
-parse :: Text -> Either (Doc a) (Input Action)
-parse t = case Text.splitOn "@" t of
-  [x, i] -> do
+parse :: Text -> Either (Doc a) (Input Concrete)
+parse t = case Text.words t of
+  [i, x] -> do
     n <- parseId i
-    r <- parseVal x ++ parseLabel x --XXX: should be `<|>`, but we've got some strange import of `Error` getting in the way
-    ok <| Send n r
+    map (ISelect n) (parseLabel x) ++ map (IEnter n) (parseConcrete x) --NOTE: should be `<|>`, but we've got some strange import of `Error` getting in the way
+  [x] -> do
+    l <- parseLabel x
+    ok <| IPreselect l
   ["help"] -> throw usage
   ["h"] -> throw usage
   _ -> throw <| sep ["!!", Pretty.dquotes (pretty t), "is not a valid command, type `help` for more info"]
