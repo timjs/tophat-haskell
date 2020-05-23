e9f44dc009d70d535e553a82029f11bf0d89d592
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2020-05-23 08:42:36 +0200

    add distinction between Global and Local heap

--- src/Polysemy/Mutate.hs
+++ src/Polysemy/Mutate.hs
@@ -6,6 +6,7 @@ module Polysemy.Mutate
     Write (..),
     Alloc (..),
     Mutate,
+    Heap (..),
 
     -- * Actions
     read,
@@ -34,16 +35,26 @@ import Data.STRef (STRef, newSTRef, readSTRef, writeSTRef)
 import Polysemy
 import Polysemy.Bundle
 
+-- Heap ------------------------------------------------------------------------
+
+data Heap h
+  = Global
+  | Local h
+
+type family Ref h where
+  Ref ('Global) = IORef
+  Ref ('Local h') = STRef h'
+
 -- Effects ---------------------------------------------------------------------
 
-data Read l m a where
-  Read :: l a -> Read l m a
+data Read h m a where
+  Read :: Ref h a -> Read h m a
 
-data Write l m a where
-  Write :: a -> l a -> Write l m ()
+data Write h m a where
+  Write :: a -> Ref h a -> Write h m ()
 
-data Alloc l m a where
-  Alloc :: a -> Alloc l m (l a)
+data Alloc h m a where
+  Alloc :: a -> Alloc h m (Ref h a)
 
 makeSem ''Read
 makeSem ''Write
@@ -51,7 +62,7 @@ makeSem ''Alloc
 
 -- Operators -------------------------------------------------------------------
 
-mutate :: Members '[Read l, Write l] r => (a -> a) -> l a -> Sem r ()
+mutate :: Members '[Read h, Write h] r => (a -> a) -> Ref h a -> Sem r ()
 mutate f r = do
   x <- read r
   write (f x) r
@@ -60,54 +71,54 @@ infixl 1 <<-
 
 infixl 1 <<=
 
-(<<-) :: Members '[Read l, Write l] r => l a -> a -> Sem r ()
+(<<-) :: Members '[Read h, Write h] r => Ref h a -> a -> Sem r ()
 (<<-) = flip write
 
-(<<=) :: Members '[Read l, Write l] r => l a -> (a -> a) -> Sem r ()
+(<<=) :: Members '[Read h, Write h] r => Ref h a -> (a -> a) -> Sem r ()
 (<<=) = flip mutate
 
 -- Bundle ----------------------------------------------------------------------
 
-type Mutate l = Bundle '[Read l, Write l, Alloc l]
+type Mutate h = Bundle '[Read h, Write h, Alloc h]
 
-read' :: (Member (Read l) r', Member (Bundle r') r) => l a -> Sem r a
+read' :: (Member (Read h) r', Member (Bundle r') r) => Ref h a -> Sem r a
 read' = sendBundle << read
 
-write' :: (Member (Write l) r', Member (Bundle r') r) => a -> l a -> Sem r ()
+write' :: (Member (Write h) r', Member (Bundle r') r) => a -> Ref h a -> Sem r ()
 write' val = sendBundle << write val
 
-alloc' :: (Member (Alloc l) r', Member (Bundle r') r) => a -> Sem r (l a)
+alloc' :: (Member (Alloc h) r', Member (Bundle r') r) => a -> Sem r (Ref h a)
 alloc' = sendBundle << alloc
 
--- mutate' :: (Members '[Read l, Write l] r', Member (Bundle r') r) => (a -> a) -> l a -> Sem r ()
+-- mutate' :: (Members '[Read h, Write h] r', Member (Bundle r') r) => (a -> a) -> Ref h a -> Sem r ()
 -- mutate' f = sendBundle << mutate f
 
 -- Interpretations -------------------------------------------------------------
 
 -- In IO --
 
-readToIO :: Member (Embed IO) r => Sem (Read IORef ': r) a -> Sem r a
+readToIO :: Member (Embed IO) r => Sem (Read 'Global ': r) a -> Sem r a
 readToIO = interpret \case
   Read loc -> embed <| readIORef loc
 
-writeToIO :: Member (Embed IO) r => Sem (Write IORef ': r) a -> Sem r a
+writeToIO :: Member (Embed IO) r => Sem (Write 'Global ': r) a -> Sem r a
 writeToIO = interpret \case
   Write val loc -> embed <| writeIORef loc val
 
-allocToIO :: Member (Embed IO) r => Sem (Alloc IORef ': r) a -> Sem r a
+allocToIO :: Member (Embed IO) r => Sem (Alloc 'Global ': r) a -> Sem r a
 allocToIO = interpret \case
   Alloc val -> embed <| newIORef val
 
 -- In ST --
 
-readToST :: Member (Embed (ST h)) r => Sem (Read (STRef h) ': r) a -> Sem r a
+readToST :: Member (Embed (ST h)) r => Sem (Read ('Local h) ': r) a -> Sem r a
 readToST = interpret \case
   Read loc -> embed <| readSTRef loc
 
-writeToST :: Member (Embed (ST h)) r => Sem (Write (STRef h) ': r) a -> Sem r a
+writeToST :: Member (Embed (ST h)) r => Sem (Write ('Local h) ': r) a -> Sem r a
 writeToST = interpret \case
   Write val loc -> embed <| writeSTRef loc val
 
-allocToST :: Member (Embed (ST h)) r => Sem (Alloc (STRef h) ': r) a -> Sem r a
+allocToST :: Member (Embed (ST h)) r => Sem (Alloc ('Local h) ': r) a -> Sem r a
 allocToST = interpret \case
   Alloc val -> embed <| newSTRef val
