72acf78f2b854579a90c7b7f4ad175d63d58f70f
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2020-12-29 18:51:43 +0100

    add example of troubled future selection

--- src/Data/Basic.hs
+++ src/Data/Basic.hs
@@ -9,7 +9,7 @@ where
 -- |
 -- | They should be:
 -- | * `Display` to show them in a user interface.
--- | * `Parse` to parse them from input.
+-- | * `Scan` to parse them from input.
 -- | * `Reflect` to match input type and editor type witch each other.
 -- |
 -- | It is defined as a class synonym instead of a type synonym to let GHC
